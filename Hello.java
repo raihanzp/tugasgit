0de8a561842fc82fc18940b2b01a1d72a4373b5a
raihanzp <77537742+raihanzp@users.noreply.github.com>
2021-01-18 22:30:51 +0700

    Fifth Commit

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ class Hello{
 		public static void main (String[] args){
 		System.out.println("Contoh")
 		System.out.println("RD : Aplikasi Java 2021");
-		System.out.println("---***---")
+		System.out.println("---***---");
+		System.out.println("END")
 		}
 	}
