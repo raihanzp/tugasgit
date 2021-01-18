9fca42a5d76ce97560f5eed56de9024e6f68383e
RD <raihanprayogi48@gmil.com>
2021-01-18 22:29:06 +0700

    Fourth Commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 class Hello{
 		public static void main (String[] args){
+		System.out.println("Contoh")
 		System.out.println("RD : Aplikasi Java 2021");
+		System.out.println("---***---")
 		}
 	}
